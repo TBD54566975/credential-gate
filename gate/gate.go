6064fbdb050695e7fd9e0b89e34ebedee678944b
gabe <gcohen@squareup.com>
2023-04-05 15:54:59 -0700

    optionality

--- gate/gate.go
+++ gate/gate.go
@@ -1,7 +1,9 @@
 package gate
 
 import (
+	"github.com/TBD54566975/ssi-sdk/credential"
 	"github.com/TBD54566975/ssi-sdk/credential/exchange"
+	"github.com/TBD54566975/ssi-sdk/credential/signing"
 	didsdk "github.com/TBD54566975/ssi-sdk/did"
 	"github.com/TBD54566975/ssi-sdk/util"
 
@@ -27,6 +29,8 @@ type CredentialGate struct {
 	config   CredentialGateConfig
 }
 
+// NewCredentialGate creates a new CredentialGate instance using the given config
+// which is used to validate credentials against the given presentation definition
 func NewCredentialGate(config CredentialGateConfig) (*CredentialGate, error) {
 	if err := util.IsValidStruct(config); err != nil {
 		return nil, util.LoggingErrorMsg(err, "invalid config")
@@ -41,4 +45,39 @@ func NewCredentialGate(config CredentialGateConfig) (*CredentialGate, error) {
 		resolver: r,
 		config:   config,
 	}, nil
+}
+
+// Presentation is a struct that contains sets of VPs and/or VP JWTs, both of which can be used
+// that are to be validated against the credential gate's presentation definition
+type Presentation struct {
+	VerifiablePresentation    *credential.VerifiablePresentation `json:"presentation,omitempty"`
+	VerifiablePresentationJWT *string                            `json:"presentationJwt,omitempty"`
+}
+
+func (p Presentation) IsValid() bool {
+	bothEmpty := p.VerifiablePresentation.IsEmpty() && p.VerifiablePresentationJWT == nil
+	bothPresent := !p.VerifiablePresentation.IsEmpty() && p.VerifiablePresentationJWT != nil
+	return !bothEmpty && !bothPresent
+}
+
+func (cg *CredentialGate) ValidatePresentation(presentation Presentation) (bool, error) {
+	if !presentation.IsValid() {
+		return false, util.LoggingErrorMsg(nil, "invalid presentation")
+	}
+
+	// handle having a VP
+	if !presentation.VerifiablePresentation.IsEmpty() {
+		return false, util.LoggingErrorMsg(nil, "Linked Data VP not yet supported")
+	}
+
+	// handle having a VP JWT
+	if presentation.VerifiablePresentationJWT != nil {
+		vpJWT, err := signing.ParseVerifiablePresentationFromJWT(*presentation.VerifiablePresentationJWT)
+		if err != nil {
+			return false, util.LoggingErrorMsg(err, "failed to parse VP JWT")
+		}
+		vp = *vpJWT
+	}
+
+	return false, nil
 }
