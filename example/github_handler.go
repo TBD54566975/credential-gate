c01e6b1d779cc02601add561b08534d93f9cfb3e
gabe <gcohen@squareup.com>
2023-04-24 21:04:12 -0700

    add github cred handler

--- example/github_handler.go
+++ example/github_handler.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/TBD54566975/ssi-sdk/credential"
 	"github.com/TBD54566975/ssi-sdk/credential/exchange"
+	"github.com/pkg/errors"
 
 	"github.com/TBD54566975/credential-gate/gate"
+)
+
+const (
+	GitHubCredentialType string = "GitHubCredential"
 )
 
 // NewGitHubHandler returns a new GitHub handler
@@ -35,6 +44,53 @@ func NewGitHubHandler(inputDescriptorID string) gate.CustomHandler {
 	}
 }
 
-func githubHandler(ctx context.Context, vsd exchange.VerifiedSubmissionData) (bool, error) {
-	return false, nil
+func githubHandler(_ context.Context, vsd exchange.VerifiedSubmissionData) (bool, error) {
+	_, _, cred, err := credential.ToCredential(vsd.Claim)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to parse credential before checking invalid after")
+	}
+	if !hasGitHubCredentialType(*cred) {
+		return false,
+			errors.New("credential is not of type GitHubCredential")
+	}
+
+	// get file from github and check it against the credential subject
+	credSubject, ok := cred.CredentialSubject[credential.VerifiableCredentialIDProperty].(string)
+	if !ok {
+		return false, errors.New("credential subject does not have ID property")
+	}
+
+	// get gist property
+	gistURL, ok := vsd.FilteredData.(string)
+	if !ok {
+		return false, errors.New("credential subject does not have gist property")
+	}
+
+	resp, err := http.Get(gistURL)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get gist")
+	}
+	defer resp.Body.Close()
+	// read body into a string
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to read gist body")
+	}
+	if string(body) != credSubject {
+		return false, fmt.Errorf("gist<%s> does not contain the credential subject<%s>", string(body), credSubject)
+	}
+	return true, nil
+}
+
+func hasGitHubCredentialType(cred credential.VerifiableCredential) bool {
+	types, ok := cred.Type.([]any)
+	if !ok {
+		return false
+	}
+	for _, t := range types {
+		if t == GitHubCredentialType {
+			return true
+		}
+	}
+	return false
 }
--- example/github_handler_test.go
+++ example/github_handler_test.go
@@ -1,9 +1,121 @@
 package main
 
 import (
+	"context"
 	"testing"
+	"time"
+
+	"github.com/TBD54566975/ssi-sdk/credential"
+	"github.com/TBD54566975/ssi-sdk/credential/exchange"
+	"github.com/TBD54566975/ssi-sdk/crypto"
+	"github.com/TBD54566975/ssi-sdk/did"
+	"github.com/TBD54566975/ssi-sdk/util"
+	"github.com/google/uuid"
+	"github.com/mr-tron/base58"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/TBD54566975/credential-gate/gate"
 )
 
-func TestGitHubHandler(t *testing.T) {
+var (
+	pubKeyB58  = "CKVrGgFtpyhpcMNpLZn3Am1DCgnbpTh4EvDbkR49hU2J"
+	privKeyB58 = "5aWH9Fd1VyGnzyQSLDCDwgdL5LTFz4P13VbPvXR8HsumB14HesiuA3fDZfVWybmdv94j7aNwnB55WoQtVHQrRbre"
+)
 
+// TestGitHubHandlerIntegration tests the GitHub handler - this is an integration test
+func TestGitHubHandlerIntegration(t *testing.T) {
+	requesterID := "did:test:admin"
+	presentationDefinition := exchange.PresentationDefinition{
+		ID: uuid.New().String(),
+		Format: &exchange.ClaimFormat{
+			JWTVP: &exchange.JWTType{
+				Alg: []crypto.SignatureAlgorithm{crypto.EdDSA},
+			},
+		},
+		InputDescriptors: []exchange.InputDescriptor{
+			{
+				ID: uuid.New().String(),
+				Format: &exchange.ClaimFormat{
+					JWTVC: &exchange.JWTType{Alg: []crypto.SignatureAlgorithm{crypto.EdDSA}},
+				},
+				Constraints: &exchange.Constraints{
+					Fields: []exchange.Field{
+						{
+							Path: []string{"$.vc.type"},
+						},
+						{
+							Path: []string{"$.vc.credentialSubject.gist"},
+							Filter: &exchange.Filter{
+								Type:    "string",
+								Pattern: "^https://gist.githubusercontent.com/.*",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	t.Run("Test GitHub Handler", func(tt *testing.T) {
+		// reconstruct pub key and priv key
+		pubKeyBytes, err := base58.Decode(pubKeyB58)
+		assert.NoError(tt, err)
+
+		privKeyBytes, err := base58.Decode(privKeyB58)
+		assert.NoError(tt, err)
+		privKey, err := crypto.BytesToPrivKey(privKeyBytes, crypto.Ed25519)
+		assert.NoError(tt, err)
+
+		didKey, err := did.CreateDIDKey(crypto.Ed25519, pubKeyBytes)
+		assert.NoError(tt, err)
+		knownDID, err := didKey.Expand()
+		assert.NoError(tt, err)
+		knownVerificationMethod := knownDID.VerificationMethod[0].ID
+
+		// add new custom handler with a valid input descriptor ID
+		inputDescriptorID := presentationDefinition.InputDescriptors[0].ID
+		g, err := gate.NewCredentialGate(gate.CredentialGateConfig{
+			AdminDID:               requesterID,
+			PresentationDefinition: presentationDefinition,
+			CustomHandlers: map[string]gate.CustomHandler{
+				inputDescriptorID: NewGitHubHandler(inputDescriptorID),
+			},
+		})
+		assert.NoError(tt, err)
+
+		signer, err := crypto.NewJWTSigner(knownDID.ID, knownVerificationMethod, privKey)
+		assert.NoError(tt, err)
+
+		testCred := getTestCredential(knownDID.ID)
+		testVCJWT, err := credential.SignVerifiableCredentialJWT(*signer, testCred)
+		assert.NoError(tt, err)
+
+		presentationClaimJWT := exchange.PresentationClaim{
+			Token:                         util.StringPtr(string(testVCJWT)),
+			JWTFormat:                     exchange.JWTVC.Ptr(),
+			SignatureAlgorithmOrProofType: string(crypto.EdDSA),
+		}
+
+		// build a presentation submission to match
+		submissionJWT, err := exchange.BuildPresentationSubmission(*signer, requesterID,
+			presentationDefinition, []exchange.PresentationClaim{presentationClaimJWT}, exchange.JWTVPTarget)
+		assert.NoError(tt, err)
+
+		result, err := g.ValidatePresentationSubmission(context.Background(), string(submissionJWT))
+		assert.NoError(tt, err)
+		assert.True(tt, result.Valid)
+	})
+}
+
+func getTestCredential(knownDID string) credential.VerifiableCredential {
+	return credential.VerifiableCredential{
+		Context:      []any{"https://www.w3.org/2018/credentials/v1"},
+		Type:         []string{"VerifiableCredential", "GitHubCredential"},
+		Issuer:       knownDID,
+		IssuanceDate: time.Now().Format(time.RFC3339),
+		CredentialSubject: map[string]any{
+			"id":   knownDID,
+			"gist": "https://gist.githubusercontent.com/decentralgabe/7cd5e914420db14324569bc45a266ad5/raw/6a8ceecf7edd4ac524dec3c5825e0bb7484f77ab/my-did-credgate.txt",
+		},
+	}
 }
